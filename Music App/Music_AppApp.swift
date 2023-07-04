9f18838e1cccf6cd5ccd43c373d1ee643da78949
Sanskar Mishra <sanskarmishra@gmail.com>
2023-07-04 15:53:42 -0500

    Initial Commit

---
+++ Music App/Music_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_AppApp.swift
+//  Music App
+//
+//  Created by Sanskar Mishra on 7/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Music_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
